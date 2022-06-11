ffbee108b86a79ff94dfb3fe5ac0865348a97af2
Mohamed Diagne <mohameddiagne@MacBook-Air-de-Mohamed.local>
2022-06-11 19:27:32 +0200

    Log

--- tp1.py
+++ tp1.py
@@ -2,12 +2,28 @@ import socket
 import time
 import datetime
 import json
-localIP     = "10.192.50.77"
+import csv
+import os
+
+
+localIP     = "192.168.56.9"
+myIP     = "192.168.56.1"
 localPort   = 3000
 Portmachine   = 4200
 bufferSize  = 1024
-msgFromServer  = "120"
-bytesToSend =  bytes.fromhex("12")
+msgFromServer  = "12"
+bytesToSend =  bytes.fromhex(msgFromServer)
+idinitialiaze=0
+idTp=1
+null_= "NaN"
+v1_="v1"
+time_data = 0
+
+
+listelogname = ["Time","IdTest","Action","RepId","lenghtString","Attendu","Observe","Verdic", "Message","VersionOutil"]
+listeinit = []
+listedata = []
+
 
 
 # Opening JSON file
@@ -18,66 +34,112 @@ def openconfigfile():
     return stringjson
 
 def initializeUT(UDPServerSocket):
-    print("initialisagion de UT")
+    print("initialisation de UT")
     successut = 1  ### x01 en entier est 1   1 == succes 0 == echoue
-    UDPServerSocket.sendto(bytes.fromhex("00"), (localIP, 4200))
+    msginitialiaze=bytes.fromhex("00") # idtest initialiaze = 0
+    UDPServerSocket.sendto(msginitialiaze, (localIP, 4200))
+    time_= datetime.datetime.fromtimestamp(time.time())
     try:
         returnUT = UDPServerSocket.recvfrom(bufferSize)
     except socket.timeout:
-        return "inconc"
+        initverdic="inconc"
+        resultinc=[time_,idinitialiaze,msginitialiaze,null_,null_,null_,null_,initverdic,null_,v1_]
+        listeinit.extend(resultinc)
+        return initverdic
     
-    code = returnUT[0][1:2]
-    if successut.__eq__(int.from_bytes(code, "big")) :
+    coderp = returnUT[0][1:2]
+    repid = returnUT[0][:1]
+    initmsgreturn_=returnUT[0]
+    resultreq=[time_,idinitialiaze,msginitialiaze,repid,null_,coderp]
+    listeinit.extend(resultreq)
+    if successut.__eq__(int.from_bytes(coderp, "big")) :
         print("initialisation UT reussite")
-        return "success"
+        initsucess="success"
+        initverdic="pass"
+        resultsuccess= [initsucess,initverdic,initmsgreturn_,v1_]
+        listeinit.extend(resultsuccess)
+        return initsucess
     else:
         print("initialisation UT echoué")
-        return "fail"
+        initfail="erreur"
+        initverdic="fail"
+        resulterror = [initfail,initverdic,initmsgreturn_,v1_]
+        listeinit.extend(resulterror)
+        return initfail
 
 
 def initiateDatagram():
     # Create a datagram socket
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     # Bind to address and ip
-    UDPServerSocket.bind((localIP, localPort))
+    UDPServerSocket.bind((myIP, localPort))
     return UDPServerSocket
 
-def sendto(UDPServerSocket):
+def sendto_(UDPServerSocket):
     UDPServerSocket.sendto(bytesToSend, (localIP, 4200))
 
 def recerivefrom(UDPServerSocket):
     print("UDP server up and listening")
+    global time_data
+    time_data= datetime.datetime.fromtimestamp(time.time())
     try:
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     except socket.timeout:
-        return "inconc"
+        initverdic="inconc"
+        resultinc=[time_data,idinitialiaze,bytesToSend,null_,null_,null_,null_,initverdic,null_,v1_]
+        listedata.extend(resultinc)
+        return initverdic
     return bytesAddressPair
 
 def decode(bytesAddressPair,stringjson):
-    print(datetime.datetime.fromtimestamp(time.time()),bytesAddressPair[0])
-    code = bytesAddressPair[0][:1]
+    #print(datetime.datetime.fromtimestamp(time.time()),bytesAddressPair[0])
+    repid = bytesAddressPair[0][:1]
     lent = bytesAddressPair[0][1:2]
     split3 = bytesAddressPair[0][2:]
+    msgreturn_=bytesAddressPair[0]
     stringresult = split3.decode("utf-8") 
     lent = int.from_bytes(lent, "big")  
-    code = int.from_bytes(code, "big")  ### equivalent de x11 est 17 en entier   
-    if stringresult.__eq__(stringjson) and code == 17 :
+    repid = int.from_bytes(repid, "big")  ### equivalent de x11 est 17 en entier
+    resultreq=[time_data,idTp,bytesToSend,repid,lent]
+    listedata.extend(resultreq)   
+    if stringresult.__eq__(stringjson) and repid == 17 :
         print('aucune boisson n\'a été selectionnée et aucun prix n\'est affiché')
-        return "sucess"
+        datasucess="success"
+        dataverdic="pass"
+        resultsuccess= [stringresult,dataverdic,msgreturn_,v1_]
+        listedata.extend(resultsuccess)
+        return datasucess
     else:
         print('une boisson est peut etre selectionné')   
-        return "fail"
+        dataverdic="fail"
+        resulterror = [dataverdic,dataverdic,msgreturn_,v1_]
+        listedata.extend(resulterror)
+        return dataverdic
+
+def logfile():
+    with open("log.xls", 'a', encoding='UTF8', newline='') as flog:
+        writer=csv.writer(flog)
+        testfile=os.stat("log.xls").st_size == 0
+        if testfile is True:
+            writer.writerow(listelogname)
+            writer.writerow(listeinit)
+            writer.writerow(listedata)
+        else:
+            writer.writerow(listeinit)
+            writer.writerow(listedata)
 
 
 if __name__ == "__main__":
     UDPServerSocket = initiateDatagram()
     resultUT= initializeUT(UDPServerSocket)
     print("initialisateur:",resultUT)
-    sendto(UDPServerSocket)
+    print(bytesToSend )
+    sendto_(UDPServerSocket)
     bytesAddressPair =recerivefrom(UDPServerSocket)
     stringjson = openconfigfile()
     resultFinal= decode(bytesAddressPair,stringjson)
     print("resultat final",resultFinal)
+    logfile()
 
 
  
